02ded5ae4741756fab55519ab53772fea4e36c52
Nick Feuer <nickfeuer@gmail.com>
2019-12-16 11:03:43 -0400

    bmp180

--- ClusterDuckTest/ClusterDuckTest.ino
+++ ClusterDuckTest/ClusterDuckTest.ino
@@ -1,28 +1,53 @@
 #include "ClusterDuck.h"
-//#include "timer.h"
+#include <Adafruit_BMP085_U.h>
+#include "timer.h"
 
-//auto timer = timer_create_default(); // create a timer with default settings
+Adafruit_BMP085_Unified bmp = Adafruit_BMP085_Unified(10085);
+auto timer = timer_create_default(); // create a timer with default settings
 
 ClusterDuck duck;
 
 void setup() {
-  //timer.tick();
-  
+
   // put your setup code here, to run once:
   duck.begin();
-  duck.setDeviceId("Z", 10);
+  duck.setDeviceId("Test", 10);
   duck.setupMamaDuck();
 
-  //timer.every(300000, runSensor);
+  if (!bmp.begin())
+  {
+    /* There was a problem detecting the BMP085 ... check your connections */
+    Serial.print("Ooops, no BMP085 detected ... Check your wiring or I2C ADDR!");
+  } else {
+    Serial.println("here");
+  }
+
+  timer.every(300000, runSensor);
 }
 
 void loop() {
+  timer.tick();
   // put your main code here, to run repeatedly:
   duck.runMamaDuck();
-  
+
 }
 
 bool runSensor(void *) {
+  float T,P;
+  String vals;
+  
+  bmp.getTemperature(&T);
+  //T = bmp.readTemperature();
+  Serial.println(T);
+  bmp.getPressure(&P);
+  //P = bmp.readPressure();
+  Serial.println(P);
 
+  Serial.println("Temp: " + String(T) + " Pres: " + String(P));
+  
+  vals = "Temp: " + String(T) + " Pres: " + String(P); //Store Data
+
+  duck.sendPayloadMessage(vals);
+  
   return true;
 }
