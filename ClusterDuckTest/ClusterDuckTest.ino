4897c9127b92d800e594a6a3501e22487c39f5c1
nfeuer <nickfeuer@gmail.com>
2020-01-30 16:36:54 -0500

    pinger and repeater code combined

--- ClusterDuckTest/ClusterDuckTest.ino
+++ ClusterDuckTest/ClusterDuckTest.ino
@@ -47,3 +47,43 @@ bool runSensor(void *) {
 
   return true;
 }
+
+//#include "ClusterDuck.h"
+//#include "timer.h"
+//
+//auto timer = timer_create_default(); // create a timer with default settings
+//
+//ClusterDuck duck;
+//byte pinger = 0xA1;
+//byte m_byte = 0xA2;
+//String senderId;
+//
+//void setup() {
+//  
+//  // put your setup code here, to run once:
+//  duck.begin();
+//  duck.setDeviceId("Pinger", 10);
+//
+//  duck.setupLoRa();
+//  duck.setupDisplay("Pinger");
+//
+//  timer.every(5000, runSensor);
+//}
+//
+//void loop() {
+//  timer.tick();
+//  
+//  // put your main code here, to run repeatedly:
+//  
+//}
+//
+//bool runSensor(void *) {
+//  LoRa.beginPacket();
+//  duck.couple(pinger, "Pinger");
+//  duck.couple(m_byte, duck.uuidCreator());
+//  LoRa.endPacket();
+//
+//  Serial.println("Packet sent");
+//  
+//  return true;
+//}
