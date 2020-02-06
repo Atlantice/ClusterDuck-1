3bf804d9249bf5a54ae5415cf09fd73f3a561781
Nick Feuer <nickfeuer@gmail.com>
2020-02-05 20:12:53 -0500

    new structure

---
+++ ClusterDuckTest/examples/DuckLinkRssi.ino
@@ -0,0 +1,42 @@
+#include "ClusterDuck.h"
+//#include "timer.h"
+
+//auto timer = timer_create_default(); // create a timer with default settings
+
+ClusterDuck duck;
+
+byte senderId = 0xF5;
+byte ping = 0xF4;
+
+void setup() {
+  
+  // put your setup code here, to run once:
+  duck.begin();
+  duck.setDeviceId("Z");
+  duck.setupLoRa();
+  LoRa.receive();
+  duck.setupDisplay("Z");
+
+  //timer.every(300000, runSensor);
+}
+
+void loop() {
+  //timer.tick();
+  
+  int packetSize = LoRa.parsePacket();
+  if (packetSize != 0) {
+    byte whoIsIt = LoRa.peek();
+    if(whoIsIt == ping) {
+      int rssi = LoRa.packetRssi();
+      String * msg = duck.getPacketData(packetSize);
+      Packet last = duck.getLastPacket();
+      duck.sendPayloadStandard(last.senderId + ":" + rssi);
+    } 
+  }
+  
+}
+
+bool runSensor(void *) {
+
+  return true;
+}
