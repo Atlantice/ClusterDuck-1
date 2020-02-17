5cb072713a317e10cc232394ec0c249eab4dc341
Taraqur Rahman <tqrahman8@gmail.com>
2020-02-17 18:35:30 -0500

    updated ducklinkrssi

--- examples/DuckLinkRssi/DuckLinkRssi.ino
+++ examples/DuckLinkRssi/DuckLinkRssi.ino
@@ -38,44 +38,4 @@ void loop() {
 bool runSensor(void *) {
 
   return true;
-}#include <ClusterDuck.h>
-//#include "timer.h"
-
-//auto timer = timer_create_default(); // create a timer with default settings
-
-ClusterDuck duck;
-
-byte senderId = 0xF5;
-byte ping = 0xF1;
-
-void setup() {
-  
-  // put your setup code here, to run once:
-  duck.begin();
-  duck.setDeviceId("C1");
-  duck.setupLoRa();
-  LoRa.receive();
-  duck.setupDisplay("C1");
-
-  //timer.every(300000, runSensor);
-}
-
-void loop() {
-  //timer.tick();
-  
-  int packetSize = LoRa.parsePacket();
-  if (packetSize != 0) {
-    byte whoIsIt = LoRa.peek();
-    if(whoIsIt == ping) {
-      int rssi = LoRa.packetRssi();
-      String * msg = duck.getPacketData(packetSize);
-      duck.sendPayloadStandard(msg[0] + ":" + rssi);
-    } 
-  }
-  
-}
-
-bool runSensor(void *) {
-
-  return true;
 }
